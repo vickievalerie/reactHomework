8710a79951a91e71aa1327102bb91b4a2d49cb0f
Vickie Soshnikova <vickie@soshnikov.com>
2025-01-16 02:12:36 +0300

    add search filter

--- recipeApp/screens/Home.jsx
+++ recipeApp/screens/Home.jsx
@@ -7,8 +7,12 @@ import {
   ActivityIndicator,
   RefreshControl,
   TouchableOpacity,
+  StyleSheet,
+  TextInput,
 } from 'react-native';
 import { Post } from '../components/Post';
+import Icon from "react-native-vector-icons/FontAwesome";
+
 
 const recipes = require('../assets/recipes.json');
 
@@ -16,6 +20,12 @@ const recipes = require('../assets/recipes.json');
 export const HomeScreen = ({ navigation }) => {
   const [isLoading, setIsLoading] = React.useState(true);
   const [items, setItems] = React.useState();
+  const [search, setSearch] = React.useState(null);
+
+  const filterList = (item) => {
+    const newList = recipes.recipes.filter((val) => val.searchIngredients.join(' ').toLocaleLowerCase().includes(item.toLocaleLowerCase()));
+    setItems(newList);
+  }
 
   const fetchPosts = () => {
     setIsLoading(true);
@@ -23,7 +33,16 @@ export const HomeScreen = ({ navigation }) => {
     setIsLoading(false);
   };
 
-  React.useEffect(fetchPosts, []);
+  React.useEffect(() => {
+    if(search !== null) {
+      filterList(search);
+    } 
+    else {
+      fetchPosts();
+    }
+  }, [search]);
+
+  // React.useEffect(fetchPosts, []);
 
   if (isLoading) {
     return (
@@ -41,6 +60,18 @@ export const HomeScreen = ({ navigation }) => {
 
   return (
     <View>
+
+      <View style={styles.search}>
+        <View style={styles.icon}>
+          <Icon name="search" size={20} color='#FFFFFF' />
+        </View>
+        <TextInput
+          style={styles.input}
+          placeholder="Search..."
+          onChangeText={(val) => setSearch(val)}
+        />
+      </View>
+
       <FlatList
         refreshControl={<RefreshControl refreshing={isLoading} onRefresh={fetchPosts} />}
         data={items}
@@ -53,4 +84,37 @@ export const HomeScreen = ({ navigation }) => {
       />
     </View>
   );
+
+  
 };
+
+const styles = StyleSheet.create({
+  search: {
+    backgroundColor: '#EBE9E9',
+    margin: 10,
+    borderRadius: 5,
+    flexDirection: 'row',
+  },
+  icon: {
+    backgroundColor: '#004c4c',
+    padding: 15,
+    borderTopLeftRadius: 5,
+    borderBottomLeftRadius: 5,
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    padding: 10,
+  },
+  listItem: {
+    marginHorizontal: 10,
+    marginVertical: 5,
+    backgroundColor: '#F3F0F0',
+    padding: 10,
+    borderRadius: 5,
+  },
+  listText: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+})
