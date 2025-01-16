1da59165692ae13fc15281a99bb11150a60ad0ba
Vickie Soshnikova <vickie@soshnikov.com>
2025-01-16 18:48:38 +0300

    final tracker

--- trackerApp/my-app/components/form.js
+++ trackerApp/my-app/components/form.js
@@ -1,30 +1,36 @@
 import React, { useState } from "react";
 import { StyleSheet, TextInput, Text, Button, View, TouchableOpacity } from "react-native";
 
-
-export default function Form({addHandler}){
-    const[text, setValue] = useState("");
+export default function Form({ addHandler }) {
+    const [text, setValue] = useState("");
+  
     const onChange = (text) => {
-        setValue(text)
-    }
-
-    return(
-        <View>
-            <TextInput style = {styles.input} onChangeText = {onChange} placeholder="Впишите задачу"/>
-            {/* <Button
-            style = {styles.button}
-            onPress={() => addHandler(text)}
-            title="Добавить задачу..."/> */}
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => addHandler(text)}>
-                <Text style={styles.buttonText}>Добавить задачу</Text>
-            </TouchableOpacity>
-
-        </View>
+      setValue(text);
+    };
+  
+    const handleAddTask = () => {
+      addHandler(text);
+      setValue("");
+    };
+  
+    return (
+      <View>
+        <TextInput 
+          style={styles.input} 
+          onChangeText={onChange} 
+          value={text}
+          placeholder="Впишите задачу"
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleAddTask}
+        >
+          <Text style={styles.buttonText}>Добавить задачу</Text>
+        </TouchableOpacity>
+      </View>
     );
-}
-
+  }
+  
 const styles = StyleSheet.create({
     button: {
         borderRadius:5,
